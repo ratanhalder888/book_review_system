be8bf0cf5134aa7bfa53b6f429df3f976372a8a1
ratan <ratanhalder60@gmail.com>
2025-10-28 12:56:59 +0600

    Add Bootstrap widgets in book form, create view for book form, add urls.py file in book app

--- book/form.py
+++ book/form.py
@@ -1,8 +1,36 @@
 from django import forms
 from book.models import Book
 
-class Bookform(forms.ModelForm):
+class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'description', 'genre',
                   'isbn', 'publication_date']
+
+    widgets = {
+        'title' : forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Book Title'
+        }),
+        'author' : forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Author Name'
+        }),
+        'description' : forms.Textarea(attrs={
+            'class': 'form-control',
+            'row': 4,
+            'placeholder': 'Enter Book Description'
+        }),
+        'genre' : forms.Select(attrs={
+            'class': 'form-control',
+        }),
+        'isbn' : forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter ISBN'
+        }),
+        'publication_date' : forms.DateInput(attrs={
+            'class': 'form-control',
+            'type': 'date'
+        }),
+
+    }
--- book/views.py
+++ book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from book.form import BookForm
+from book.models import Book
 # Create your views here.
+
+def book_create(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = BookForm()
+    return render(request, 'book/book_form.html', {'form': form})

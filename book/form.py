ea51d51ae79a5d2dce3effe6c7ed53aa24d6239e
ratan <ratanhalder60@gmail.com>
2025-10-28 17:43:29 +0600

    edit in book_form.html,add function in book/view.py book_list, book_detail, book_update

--- book/form.py
+++ book/form.py
@@ -7,30 +7,30 @@ class BookForm(forms.ModelForm):
         fields = ['title', 'author', 'description', 'genre',
                   'isbn', 'publication_date']
 
-    widgets = {
-        'title' : forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter Book Title'
-        }),
-        'author' : forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter Author Name'
-        }),
-        'description' : forms.Textarea(attrs={
-            'class': 'form-control',
-            'row': 4,
-            'placeholder': 'Enter Book Description'
-        }),
-        'genre' : forms.Select(attrs={
-            'class': 'form-control',
-        }),
-        'isbn' : forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter ISBN'
-        }),
-        'publication_date' : forms.DateInput(attrs={
-            'class': 'form-control',
-            'type': 'date'
-        }),
+        widgets = {
+            'title' : forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Book Title'
+            }),
+            'author' : forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Author Name'
+            }),
+            'description' : forms.Textarea(attrs={
+                'class': 'form-control',
+                'row': 4,
+                'placeholder': 'Enter Book Description'
+            }),
+            'genre' : forms.Select(attrs={
+                'class': 'form-control',
+            }),
+            'isbn' : forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter ISBN'
+            }),
+            'publication_date' : forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date'
+            }),
 
-    }
+        }
--- book/views.py
+++ book/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from book.form import BookForm
 from book.models import Book
+from django.contrib import messages
 # Create your views here.
 
 def book_create(request):
     if request.method == 'POST':
-        pass
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Book created successfully!')
+            return redirect('book-create')
     else:
         form = BookForm()
+    return render(request, 'book/book_form.html', {'form': form})
+
+def book_list(request):
+    books = Book.objects.all().order_by('-created_at')
+    return render(request, 'book/book_list.html', {'books':books})
+
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    return render(request, 'book/book_details.html', {'book': book})
+
+
+def book_update(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Book updated successfully!')
+            return redirect('book-list')
+    else:
+        form = BookForm(instance=book)
     return render(request, 'book/book_form.html', {'form': form})
